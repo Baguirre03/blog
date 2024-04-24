25d7e795a3eb214ce159ef4d28179168a4104ac3
Ben Aguirre <baguirre@luc.edu>
2024-04-24 16:01:25 -0500

    working fetchgit add .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
+async function grabAPI(backslash) {
+  try {
+    const response = await fetch(`https://blog-api-top.fly.dev/${backslash}`, {mode: 'cors', method: 'GET'})
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
 function App() {
+  const [posts, setPosts] = useState(null)
+  
+  useEffect(() => {
+    console.log('RUNNING')
+    const fetchData = async () => {
+      const data = await grabAPI('posts');
+      setPosts(data.allArticles)
+    };
+    fetchData();
+  }, []);
+
+  console.log(posts)
   return (
-    <h1>Welcome to BLOG API</h1>
+    posts != null ? posts.map((info) => {
+      return <h1 key={info._id}>{info.user.username} wrote: {info.text}</h1>
+    }) : <h1>Loading</h1>
   )
 }
 
